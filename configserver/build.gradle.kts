e632aed4c301ca3f58a08481e7109135f49e7b5c
Omar Hammad <omar.hammad@student.kdg.be>
2025-04-01 20:50:23 +0200

    jib task in the config server

--- configserver/build.gradle.kts
+++ configserver/build.gradle.kts
@@ -1,6 +1,9 @@
+import java.io.ByteArrayOutputStream
+
 plugins {
 	java
 	id("org.springframework.boot") version "3.4.1"
+	id("com.google.cloud.tools.jib") version "3.3.2"
 	id("io.spring.dependency-management") version "1.1.7"
 }
 
@@ -39,4 +42,54 @@ dependencyManagement {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+
+// Use project.name to determine the microservice name
+val repoName = project.name + "_microservice"
+
+// Execute the bash script before the jib task
+tasks.register("incrementTag") {
+	doFirst {
+		println("Running incrementTag task...")
+		val output = ByteArrayOutputStream()
+		exec {
+			commandLine("bash", "fetch-latest-tag.sh", repoName)
+			standardOutput = output
+		}
+		val newTag = output.toString().trim()
+		println("New tag from script: $newTag")
+		project.ext.set("newTag", newTag)
+		println("newTag property set to: ${project.ext.get("newTag")}")
+	}
+
+}
+
+tasks.named("jib") {
+	dependsOn("incrementTag")
+	doFirst {
+		println("Running jib task...")
+		if (!project.ext.has("newTag")) {
+			throw IllegalStateException("newTag property is not set. Ensure the incrementTag task runs successfully.")
+		}
+		val newTag = project.ext.get("newTag") as String
+		println("Using tag: $newTag")
+
+		jib {
+			from {
+				image = "openjdk:22-jdk-slim"
+			}
+			to {
+				image = "omarhammad97/${repoName}:$newTag"
+				tags = setOf("latest")
+				auth {
+					username = System.getenv("DOCKER_USERNAME") ?: throw GradleException("DOCKER_USERNAME not set")
+					password = System.getenv("DOCKER_PASSWORD") ?: throw GradleException("DOCKER_PASSWORD not set")
+				}
+				container {
+					mainClass = "com.omarhammad.configserver.ConfigserverApplication"
+				}
+			}
+		}
+	}
 }
